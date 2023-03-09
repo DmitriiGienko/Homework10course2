4269963c268e553fc3948708a3b8368b8e72278c
Dmitrii <cunaik@mail.ru>
2023-03-09 22:49:57 +0300

    доделал 10 домашку

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 
 import java.util.Scanner;
+import java.util.function.*;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+
 
         System.out.println("______Задача 1_____");
         //решение через анонимный класс
@@ -13,7 +18,6 @@ public class Main {
             }
         };
         System.out.println("Введите целое число");
-        Scanner sc = new Scanner(System.in);
         int vol = sc.nextInt();
         System.out.println("Число " + vol + (isEven.test(vol) ? " - четное" : " - нечетное"));
 
@@ -43,70 +47,63 @@ public class Main {
 
         System.out.println("______Задача 3_____");
         //   Решение через анонимный класс.
-        Function<Double, Long> function = new Function<Double, Long>() {
-            @Override
-            public Long accept(Double a) {
-                return a.longValue();
-            }
-        };
         System.out.println("Введите число с десятичной частью");
         Double a = sc.nextDouble();
-        System.out.printf("Число %f округлили до %d\n", a, function.accept(a));
+        Function<Double, Long> function = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double aDouble) {
+                return a.longValue();
+
+            }
+        };
+        System.out.printf("Число %f округлили до %d\n", a, function.apply(a));
 
         // решение через лямбду
         System.out.println("Введите число с десятичной частью");
         Function<Double, Long> function1 = d -> d.longValue();
         Double d = sc.nextDouble();
-        System.out.printf("Число %f округлили до %d\n", d, function1.accept(d));
+        System.out.printf("Число %f округлили до %d\n", d, function1.apply(d));
 
         System.out.println("______Задача 4_____");
         //  Решение через анонимный класс.
         Supplier<Integer> supplier = new Supplier<Integer>() {
             @Override
-            public Integer getT() {
+            public Integer get() {
                 return (int) (Math.random() * 100);
             }
+
+
         };
-        System.out.println("Сгенерировали число " + supplier.getT());
+        System.out.println("Сгенерировали число " + supplier.get());
 
         // решение через лямбду
         Supplier<Integer> supplier1 = () -> (int) (Math.random() * 100);
-        System.out.println("Сгенерировали число " + supplier1.getT());
+        System.out.println("Сгенерировали число " + supplier1.get());
 
 
         System.out.println("______Задача 5_____");
 
-        /* Долго думал что и как делать...
-        В итоге написал программку можно ли пить алкоголь :)
-         */
+        System.out.println("Ваш возраст?");
 
-        System.out.println("Сколько вам лет?");
-
-        Function<Predicate<Integer>, String> ternaryOperator = isAdult ->
-                isAdult.test(sc.nextInt()) ? "Можно выпить!" : "Иди в школу";
-        System.out.println(ternaryOperator.accept(age -> age >= 18));
+        System.out.println(ternaryOperator(integer -> integer >= 18,
+                yes -> "Можно выпить",
+                no -> "Еще не дорос").apply(Function.identity()));
 
 
     }
+
+    public static <T, U> Function<? super T, ? extends U> ternaryOperator(
+            Predicate<Integer> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        if (condition.test(sc.nextInt())) {
+            return ifTrue;
+        } else {
+            return ifFalse;
+        }
+    }
+
+
 }
 
-@FunctionalInterface
-interface Predicate<T> {
-    boolean test(T t);
-}
-
-@FunctionalInterface
-interface Consumer<T> {
-    void accept(T t);
-}
-
-@FunctionalInterface
-interface Function<T, R> {
-    R accept(T t);
-}
-
-@FunctionalInterface
-interface Supplier<T> {
-    T getT();
-}
 
